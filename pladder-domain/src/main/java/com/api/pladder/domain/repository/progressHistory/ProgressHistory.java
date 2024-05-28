0261f18d0c2d46c8320f32463751fd3a1c35047b
antidote <ljh8651@nate.com>
2024-05-29 08:01:43 +0900

    feat/ progressRepository 오타수정

--- pladder-domain/src/main/java/com/api/pladder/domain/repository/progressHistory/ProgressHistory.java
+++ pladder-domain/src/main/java/com/api/pladder/domain/repository/progressHistory/ProgressHistory.java
@@ -1,6 +1,6 @@
 package com.api.pladder.domain.repository.progressHistory;
 
-import com.api.pladder.domain.entity.base.BaseEntity;
+import com.api.pladder.domain.repository.common.BaseRepository;
 
-public interface ProgressHistory extends BaseEntity<ProgressHistory,String> {
+public interface ProgressHistory extends BaseRepository<ProgressHistory,String> {
 }
