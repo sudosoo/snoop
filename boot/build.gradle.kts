d99bc52a62ff9193772b23a41f0c0db0691a286d
antidote <ljh8651@nate.com>
2024-05-31 22:31:03 +0900

    feat/ swagger jasypt 의존성 추가

--- boot/build.gradle.kts
+++ boot/build.gradle.kts
@@ -1,3 +1,6 @@
 dependencies {
     implementation(project(":pladder-presentation"))
+
+    // jasypt
+    implementation ("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
 }
--- pladder-presentation/build.gradle.kts
+++ pladder-presentation/build.gradle.kts
@@ -1,5 +1,9 @@
 dependencies {
     implementation (project(":pladder-application"))
 
+    // Springdoc OpenAPI
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 
+    // Spring Boot Starter Validation
+    implementation ("org.springframework.boot:spring-boot-starter-validation")
 }
